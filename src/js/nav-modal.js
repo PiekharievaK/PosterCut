3a000038cb9d64d59f7eb897b6d18e3e24df7b12
PiekharievaK <pechareva@gmail.com>
2023-03-11 15:23:06 +0200

    правит шапку в мобильной версии, ссылки на картинки

--- src/js/nav-modal.js
+++ src/js/nav-modal.js
@@ -1,11 +1,12 @@
 const nav = document.getElementById('header-nav');
 const button = document.getElementById('header-button');
 const burger = document.getElementsByClassName("header_burger")[0]
+const mobileControl = document.getElementById('header-mobile-control')
 
 const classToggle = () => {
   nav.classList.toggle('modal-open');
   document.body.classList.toggle('modal-open');
-  burger.classList.toggle('active')
+  mobileControl.classList.toggle('active')
 
 };
 
@@ -23,7 +24,7 @@ const headerToggle = () => {
     if (!e.matches) return;
     nav.classList.remove('modal-open');
     document.body.classList.remove('modal-open');
-    burger.classList.remove('active')
+    mobileControl.classList.remove('active')
     navLinks.forEach(item => item.removeEventListener('click', classToggle));
   });
 };
